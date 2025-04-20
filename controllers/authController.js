2b4bfe01b2b42f37be5c98e93537cca50248a01d
“kurakinanatalie” <kurakina.natalie@gmail.com>
2025-04-20 21:00:02 +0100

    ,

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,10 +13,16 @@ function login(req, res) {
       return res.redirect('/login?error=User not found');
     }
 
+    console.log('User from DB:', organiser);
+    console.log('Typed password:', password);
+    console.log('Stored hash:', organiser.password);
+
+
     bcrypt.compare(password, organiser.password, (err, match) => {
+        console.log('Password match result:', match);
       if (match) {
         req.session.user = { username: organiser.username, role: 'organiser' };
-        res.redirect('/dashboard'); // можно заменить на реальный путь позже
+        res.redirect('/dashboard');
       } else {
         res.redirect('/login?error=Incorrect password');
       }

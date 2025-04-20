ac75478578c2607fef3b10a21bdb89df00b64a55
“kurakinanatalie” <kurakina.natalie@gmail.com>
2025-04-20 21:16:10 +0100

    1

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
-
 const authRoutes = require('./routes/authRoutes');
 app.use('/', authRoutes);
 
@@ -33,7 +31,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
   secret: process.env.SESSION_SECRET || 'cat',

6a273ebc3b28dd106ed09afa402bcbc1e92ce189
“kurakinanatalie” <kurakina.natalie@gmail.com>
2025-04-22 17:06:35 +0100

    admin

--- models/user.js
+++ models/user.js
@@ -1,7 +1,18 @@
+const path = require('path');
 const Datastore = require('@seald-io/nedb');
 const bcrypt = require('bcrypt');
 
-const organiserDB = new Datastore({ filename: './data/organisers.db', autoload: true });
+const organiserDB = new Datastore({
+  filename: path.join(__dirname, '../data/organisers.db')
+});
+
+organiserDB.loadDatabase((err) => {
+  if (err) {
+    console.error('Failed to load organisers.db:', err);
+  } else {
+    console.log('organisers.db loaded');
+  }
+});
 
 function createOrganiser(username, password, callback) {
   bcrypt.hash(password, 10, (err, hash) => {
